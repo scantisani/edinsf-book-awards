114cf90279436e6da63862f1e6cc6c897e3d4bf8
Scott Cantisani <scantisani@gmail.com>
2022-06-17 02:37:41 +0100

    Initial commit.
    Used `rails new -j esbuild -c bulma -d postgresql`, then
    modified slightly to render a very simple React app
    that uses Bulma classes.
    
    Also adds Rubocop.

---
+++ app/javascript/components/application.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import {createRoot} from "react-dom/client";
+
+interface AppProps {
+  arg: string;
+}
+
+const App = ({arg}: AppProps) => {
+  return <div class="notification is-primary">{`Hello, ${arg}!`}</div>;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("container");
+  const root = createRoot(container!);
+  root.render(<App arg="Rails 7 with ESBuild"/>);
+});
