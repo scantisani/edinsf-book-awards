275d5886ea9a0dfda937098bfdbed724b9e12d79
Scott Cantisani <scantisani@gmail.com>
2022-07-06 22:12:42 +0200

    Add seeds for books read in 2021.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Book.create!([
+  { title: 'The Monk', author: 'Matthew Lewis', published_at: Time.new(1796), read_at: Time.new(2021, 1), chosen_by: 'Amy' },
+  { title: 'Plum Rains', author: 'Andromeda Romano-Lax', published_at: Time.new(2018), read_at: Time.new(2021, 2), chosen_by: 'Scott' },
+  { title: 'A Memory Called Empire', author: 'Arkady Martine', published_at: Time.new(2019), read_at: Time.new(2021, 3), chosen_by: 'Bruno' },
+  { title: 'The Memory Police', author: 'Yoko Ogawa', published_at: Time.new(1994), read_at: Time.new(2021, 4), chosen_by: 'Turner' },
+  { title: 'Jonathan Strange & Mr Norrell', author: 'Susanna Clarke', published_at: Time.new(2004), read_at: Time.new(2021, 5), chosen_by: 'Dorothy' },
+  { title: 'Before the Coffee Gets Cold', author: 'Toshikazu Kawaguchi', published_at: Time.new(2015), read_at: Time.new(2021, 6), chosen_by: 'Joe' },
+  { title: 'Infinite Detail', author: 'Tim Maughan', published_at: Time.new(2019), read_at: Time.new(2021, 7), chosen_by: 'Katy' },
+  { title: 'The Affirmation', author: 'Christopher Priest', published_at: Time.new(1981), read_at: Time.new(2021, 8), chosen_by: 'Rory' },
+  { title: "Rocannon's World", author: 'Ursula K. le Guin', published_at: Time.new(1966), read_at: Time.new(2021, 9), chosen_by: 'Tony' },
+  { title: 'The Elementals', author: 'Michael McDowell', published_at: Time.new(1981), read_at: Time.new(2021, 10), chosen_by: 'Kate' },
+  { title: 'Civilwarland in Bad Decline', author: 'George Saunders', published_at: Time.new(1996), read_at: Time.new(2021, 11), chosen_by: 'Jer' },
+])
