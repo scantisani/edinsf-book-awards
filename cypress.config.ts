4c4744f53654e11a6738cc917b8be0b30d346864
Scott Cantisani <scantisani@gmail.com>
2022-07-10 00:37:39 +0100

    Add files automatically generated by Cypress.

---
+++ cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  screenshotsFolder: "tmp/cypress_screenshots",
+  videosFolder: "tmp/cypress_videos",
+  trashAssetsBeforeRuns: false,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
